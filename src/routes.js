e33553e0f3e6d56fd83b8077c106b2f18893236b
fagnerlincoln <fagnerlincoln@outlook.com>
2024-08-09 16:43:40 -0300

    atualza dados yup

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,10 @@
 
 import {Router} from 'express'
-import {v4} from 'uuid';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+
 
 const routes = new Router()
 
-routes.get('/', async (request, response) =>{
-const user = await User.create({
-    id: v4(),
-    name: 'Lincoln',
-    email: 'faglincojesus@gmail.com',
-    password_hash: 'asdfjkl'
-});
-
- return response.status(201).json(user);
-
-});
+routes.post('/users', UserController.store);
 
 export default routes;
