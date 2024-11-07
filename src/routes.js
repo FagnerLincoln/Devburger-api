4d6deb451bfb94477e68090d135c3181ba8191bb
fagnerlincoln <fagnerlincoln@outlook.com>
2024-11-07 11:08:29 -0300

    backandfinalizado

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 
-import {Router} from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
@@ -17,6 +17,7 @@ routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
 routes.put('/products/:id', upload.single('file'), ProductController.update);
